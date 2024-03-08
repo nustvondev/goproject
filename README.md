a74dc88e5b03f73c221745848fc318f3394f2e45
Hoa Huu Nguyen <hoaboy89@gmail.com>
2024-03-08 14:22:25 +0700

    Init setup.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Config file host trong window va wsl
+
+
+Cau hinh them dong sau: ```127.0.0.1 diachimienedit.test```
+
+Duong dan trong window: ```C:\Windows\System32\drivers\etc```
+
+Doi voi wsl thi edit file hosts voi lenh sau: ```sudo nano /etc/hosts```
+
+Cau hinh traefik phan labels trong voi tien to la dia chi mien
---
+++ account/handler/handler.go
@@ -0,0 +1,80 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+}
+
+type Config struct {
+	R *gin.Engine
+}
+
+func NewHandler(c *Config) {
+	h := &Handler{}
+
+	g := c.R.Group(os.Getenv("ACCOUNT_API_URL"))
+	fmt.Println(os.Getenv("ACCOUNT_API_URL"))
+
+	g.GET("/me", h.Me)
+	g.POST("/signup", h.Signup)
+	g.POST("/signin", h.Signin)
+	g.POST("/signout", h.Signout)
+	g.POST("/tokens", h.Tokens)
+	g.POST("/image", h.Image)
+	g.DELETE("/image", h.DeleteImage)
+	g.PUT("/details", h.Details)
+}
+
+func (h *Handler) Me(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's me",
+	})
+}
+
+func (h *Handler) Signup(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's signup",
+	})
+}
+
+func (h *Handler) Signin(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's signin",
+	})
+}
+
+func (h *Handler) Signout(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's signout",
+	})
+}
+
+func (h *Handler) Tokens(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's tokens",
+	})
+}
+
+func (h *Handler) Image(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's image",
+	})
+}
+
+func (h *Handler) DeleteImage(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's deleteImage",
+	})
+}
+
+func (h *Handler) Details(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's details",
+	})
+}
